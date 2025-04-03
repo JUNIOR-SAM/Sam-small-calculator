c79302ad8d4347a13e31decee3aa86a445ac794a
samuel <oyebodes19@gmail.com>
2025-04-03 10:27:54 +0100

    this is my small calculator

---
+++ script.js
@@ -0,0 +1,51 @@
+// This is a simple JavaScript program that demonstrates the use of variables, functions, and console logging.
+// It defines a variable, a function that modifies the variable, and a function that logs the variable to the console.
+
+
+function testingTesting() {
+    console.log(showAnswer);
+}
+
+// testingTesting() //calling a function
+
+
+function sum() {
+    var firstNumber = Number(num1.value)
+    var secondNumber = Number(num2.value)
+    var sum = firstNumber + secondNumber
+    var realAnswer = "The answer is " + sum  
+
+    console.log(realAnswer)
+    showAnswer.innerHTML = realAnswer
+}
+
+function subtract() {
+    var firstNumber = (num1.value)
+    var secondNumber = (num2.value)
+    var subtract = firstNumber - secondNumber
+    var realAnswer = "The answer is " +  subtract 
+
+    console.log(realAnswer)
+    showAnswer.innerHTML = realAnswer
+}
+
+function divide() {
+    var firstNumber = num1.value
+    var secondNumber = num2.value
+    var quotient = firstNumber / secondNumber
+    var realAnswer = "The answer is " + quotient  
+
+    console.log(realAnswer)
+    showAnswer.innerHTML = realAnswer
+}
+
+function product() {
+    var firstNumber = (num1.value)
+    var secondNumber = (num2.value)
+    var multiplication = firstNumber * secondNumber
+    var realAnswer = "The answer is " +  multiplication
+
+    console.log(realAnswer)
+    showAnswer.innerHTML = realAnswer
+}
+
