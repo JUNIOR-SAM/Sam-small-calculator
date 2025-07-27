74eb911ab609e8e3faabdc07f29a05d35d7236e8
samuel <oyebodes19@gmail.com>
2025-07-27 22:02:31 +0100

    i add some style

--- script.js
+++ script.js
@@ -10,38 +10,107 @@
 
 
 function sum() {
+    if (num1.value === "" || num2.value ==="") {
+        showAnswer.innerHTML = "Please enter both numbers!";
+        showAnswer.style.backgroundColor = "red"
+        showAnswer.style.color = "white"
+        showAnswer.style.paddingTop = "17px"
+        showAnswer.style.transform = "scaleX(1.0)"
+        showAnswer.style.animation = "fadeInAnswer 0.5s";
+        setTimeout(() => { showAnswer.style.animation = ""; }, 500);
+        return;
+    }else{
+        showAnswer.style.backgroundColor = "blue"
+        showAnswer.style.color = "white"
+    }
     var firstNumber = Number(num1.value)
     var secondNumber = Number(num2.value)
     var sum = firstNumber + secondNumber
-    var realAnswer = "The answer is " + sum  
+    var realAnswer = "The answer is " + sum
 
     showAnswer.innerHTML = realAnswer
+    showAnswer.style.paddingTop = "17px"
+    showAnswer.style.animation = "fadeInAnswer 0.5s";
+    setTimeout(() => { showAnswer.style.animation = ""; }, 500);
 }
 
 function subtract() {
+        if (num1.value === "" || num2.value ==="") {
+        showAnswer.innerHTML = "Please enter both numbers!";
+        showAnswer.style.backgroundColor = "red"
+        showAnswer.style.color = "white"
+        showAnswer.style.paddingTop = "17px"
+        showAnswer.style.transform = "scaleX(1.0)"
+        showAnswer.style.animation = "fadeInAnswer 0.5s";
+        setTimeout(() => { showAnswer.style.animation = ""; }, 500);
+        return;
+    }else{
+        showAnswer.style.backgroundColor = "blue"
+        showAnswer.style.color = "white"
+    }
     var firstNumber = (num1.value)
     var secondNumber = (num2.value)
     var subtract = firstNumber - secondNumber
-    var realAnswer = "The answer is " +  subtract 
+    var realAnswer = "The answer is " + subtract
 
     showAnswer.innerHTML = realAnswer
+    showAnswer.style.paddingTop = "17px"
+    showAnswer.style.animation = "fadeInAnswer 0.5s";
+    setTimeout(() => { showAnswer.style.animation = ""; }, 500);
+
+
+
 }
 
 function divide() {
+        if (num1.value === "" || num2.value ==="") {
+        showAnswer.innerHTML = "Please enter both numbers!";
+        showAnswer.style.backgroundColor = "red"
+        showAnswer.style.color = "white"
+        showAnswer.style.paddingTop = "17px"
+        showAnswer.style.transform = "scaleX(1.0)"
+        showAnswer.style.animation = "fadeInAnswer 0.5s";
+        setTimeout(() => { showAnswer.style.animation = ""; }, 500);
+        return;
+    }else{
+        showAnswer.style.backgroundColor = "blue"
+        showAnswer.style.color = "white"
+    }
     var firstNumber = num1.value
     var secondNumber = num2.value
     var quotient = firstNumber / secondNumber
-    var realAnswer = "The answer is " + quotient  
+    var realAnswer = "The answer is " + quotient
 
     showAnswer.innerHTML = realAnswer
+    showAnswer.style.paddingTop = ""
+    showAnswer.style.animation = "fadeInAnswer 0.5s";
+    setTimeout(() => { showAnswer.style.animation = ""; }, 500);
 }
 
-function product() {
+    function product() {
+        if (num1.value === "" || num2.value ==="") {
+        showAnswer.innerHTML = "Please enter both numbers!";
+        showAnswer.style.backgroundColor = "red"
+        showAnswer.style.color = "white"
+        showAnswer.style.paddingTop = "17px"
+        showAnswer.style.transform = "scaleX(1.0)"
+        showAnswer.style.animation = "fadeInAnswer 0.5s";
+        setTimeout(() => { showAnswer.style.animation = ""; }, 500);
+        return;
+    }else{
+        showAnswer.style.backgroundColor = "blue"
+        showAnswer.style.color = "white"
+    }
     var firstNumber = (num1.value)
     var secondNumber = (num2.value)
     var multiplication = firstNumber * secondNumber
-    var realAnswer = "The answer is " +  multiplication
+    var realAnswer = "The answer is " + multiplication
 
     showAnswer.innerHTML = realAnswer
+    showAnswer.style.paddingTop = "17px"
+    showAnswer.style.animation = "fadeInAnswer 0.5s";
+    setTimeout(() => { showAnswer.style.animation = ""; }, 500);
+
+
 }
 
